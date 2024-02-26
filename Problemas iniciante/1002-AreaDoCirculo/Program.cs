886bc075388e60af7eb0576f457efaad4f399f54
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-02-26 20:54:20 -0300

    Correcao ex 1002

--- Problemas iniciante/1002-AreaDoCirculo/Program.cs
+++ Problemas iniciante/1002-AreaDoCirculo/Program.cs
@@ -6,11 +6,8 @@ class Program
     static void Main(string[] args)
     {
         double pi = 3.14159;
-        for(int i=0; i < 3; i++)
-        {
-            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double area = pi * Math.Pow(raio, 2);
-            Console.WriteLine(area.ToString("F4", CultureInfo.InvariantCulture));
-        }
+        double raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        double area = pi * Math.Pow(raio, 2);
+        Console.WriteLine($"A={ area.ToString("F4", CultureInfo.InvariantCulture)}");
     }
 }
