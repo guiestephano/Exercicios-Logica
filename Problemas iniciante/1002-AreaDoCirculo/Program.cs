57d6d2528f99fbc72e75f2321ef2ceb9abf9a987
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-02-26 20:52:29 -0300

    Exercicio 1002 - Concluido

---
+++ Problemas iniciante/1002-AreaDoCirculo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double pi = 3.14159;
+        for(int i=0; i < 3; i++)
+        {
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double area = pi * Math.Pow(raio, 2);
+            Console.WriteLine(area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
