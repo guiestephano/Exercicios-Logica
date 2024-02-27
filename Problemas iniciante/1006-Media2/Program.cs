13048278fe9292114f0f576d552c98867152c69d
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-02-26 21:35:26 -0300

    Exercicio 1006 - Concluido

---
+++ Problemas iniciante/1006-Media2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] notas = new double[3];
+        
+        for(int i = 0; i < 3; i++)
+        {
+            notas[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        double media = ((notas[0] * 2) + (notas[1] * 3) + (notas[2] * 5)) / 10;
+
+        Console.WriteLine($"MEDIA = {media.ToString("F1",CultureInfo.InvariantCulture)}");
+    }
+}
