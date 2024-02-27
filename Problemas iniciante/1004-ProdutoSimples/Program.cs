c3c1910e43220a21f8daf1010705ba6b25d7943a
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-02-26 21:10:14 -0300

    Exercicio 1004 - Concluido

---
+++ Problemas iniciante/1004-ProdutoSimples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int valorA = int.Parse(Console.ReadLine());
+        int valorB = int.Parse(Console.ReadLine());
+        int prod = valorA * valorB;
+        Console.WriteLine($"PROD = {prod}");
+    }
+}
