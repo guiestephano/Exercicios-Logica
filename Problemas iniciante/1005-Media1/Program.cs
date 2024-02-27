877165d395868819417f79e1c99417d2b63da5e0
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-02-26 21:20:17 -0300

    Exercicio 1005 - Concluido

---
+++ Problemas iniciante/1005-Media1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double valorA = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        double valorB = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        double media = ((valorA * 3.5) + (valorB * 7.5)) / 11;
+
+        Console.WriteLine($"MEDIA = {media.ToString("F5",CultureInfo.InvariantCulture)}");
+    }
+}
