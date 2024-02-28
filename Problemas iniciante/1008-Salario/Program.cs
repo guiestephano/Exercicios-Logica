421771f6504dc8dc4d67524b7876bdccb736ac70
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-02-28 01:30:34 -0300

    Exercicio 1008 - Concluido

---
+++ Problemas iniciante/1008-Salario/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = int.Parse(Console.ReadLine());
+        int horas = int.Parse(Console.ReadLine());
+        double valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) ;
+
+        Funcionario funcionario = new Funcionario(numero, horas, valorHora);
+        Console.WriteLine(funcionario.ToString());
+    }
+}
+
+class Funcionario
+{
+    public int Numero { get; private set; }
+    public int HorasTrabalhadas { get; private set; }
+    public double ValorHora { get; private set; } 
+    public double Salario { get; private set; }
+
+    public Funcionario(int numero, int horas, double valorHora)
+    {
+        Numero = numero;
+        HorasTrabalhadas = horas;
+        ValorHora = valorHora;
+        Salario = horas * valorHora;
+    }
+
+    public override string ToString()
+    {
+        return $"NUMBER = {Numero}\nSALARY = U$ {Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+    }
+}
