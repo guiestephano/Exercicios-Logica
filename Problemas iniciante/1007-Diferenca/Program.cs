9babc9570edfac7694bec7f1ad68bdb05b874456
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-02-28 01:15:54 -0300

    Exercicio 1007 -Concluido

---
+++ Problemas iniciante/1007-Diferenca/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] valores = new int[4];
+
+        for(int i = 0; i < valores.Length; i++)
+        {
+            valores[i] = int.Parse(Console.ReadLine());
+        }
+
+        int diferenca = (valores[0] * valores[1] - valores[2] * valores[3]);
+
+        Console.WriteLine($"DIFERENCA = {diferenca}");
+    }
+}
